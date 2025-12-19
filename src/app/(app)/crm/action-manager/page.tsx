40f0dfa3a2b826237712c9762375538d1ae5ad52
Dan White <dan.white@taxauditsolutions.com>
2025-12-19 17:01:56 +0000

    Try fixing this error: `Console Error: React.jsx: type is invalid -- exp

--- src/app/(app)/crm/action-manager/page.tsx
+++ src/app/(app)/crm/action-manager/page.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import {
@@ -70,28 +69,31 @@ export default function CrmActionManagerPage() {
       </header>
       
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-7xl mx-auto">
-        {crmActions.map(action => (
-          <Card key={action.title} className="flex flex-col">
-            <CardHeader>
-               <div className="flex items-center gap-4">
-                    <div className="p-3 bg-primary/10 rounded-lg">
-                        <action.icon className="h-6 w-6 text-primary" />
+        {crmActions.map(action => {
+            const Icon = action.icon;
+            return (
+              <Card key={action.title} className="flex flex-col">
+                <CardHeader>
+                   <div className="flex items-center gap-4">
+                        <div className="p-3 bg-primary/10 rounded-lg">
+                            <Icon className="h-6 w-6 text-primary" />
+                        </div>
+                        <CardTitle>{action.title}</CardTitle>
                     </div>
-                    <CardTitle>{action.title}</CardTitle>
-                </div>
-            </CardHeader>
-            <CardContent className="flex-1">
-                <CardDescription>{action.description}</CardDescription>
-            </CardContent>
-            <CardFooter>
-                 <Button asChild className="w-full">
-                  <Link href={action.href}>
-                    Go <ArrowRight className="ml-2 h-4 w-4" />
-                  </Link>
-                </Button>
-            </CardFooter>
-          </Card>
-        ))}
+                </CardHeader>
+                <CardContent className="flex-1">
+                    <CardDescription>{action.description}</CardDescription>
+                </CardContent>
+                <CardFooter>
+                     <Button asChild className="w-full">
+                      <Link href={action.href}>
+                        Go <ArrowRight className="ml-2 h-4 w-4" />
+                      </Link>
+                    </Button>
+                </CardFooter>
+              </Card>
+            );
+        })}
       </div>
     </div>
   );

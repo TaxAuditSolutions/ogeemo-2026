a4e1edb72138e14379a29394a757de47c667f6de
Dan White <dan.white@taxauditsolutions.com>
2025-12-19 12:34:06 +0000

    Lets find out if the problems of implementing changes are related to the

--- src/app/(app)/sandbox/page.tsx
+++
@@ -1,7 +0,0 @@
-// This page has been moved to /bug-repair to avoid naming conflicts.
-// This file can be safely deleted.
-import { redirect } from 'next/navigation';
-
-export default function SandboxRedirectPage() {
-  redirect('/bug-repair');
-}

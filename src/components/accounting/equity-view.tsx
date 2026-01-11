08d9ce7515a911a1a6b8d82bf25cf38aa33f2e1a
Dan White <dan.white@taxauditsolutions.com>
2026-01-11 13:17:51 +0000

    Proceed with fixing Equity Transactions as in 3.

--- src/components/accounting/equity-view.tsx
+++ src/components/accounting/equity-view.tsx
@@ -1,7 +1,7 @@
 
-"use client";
+'use client';
 
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect, useMemo, useCallback } from "react";
 import {
   Table,
   TableBody,

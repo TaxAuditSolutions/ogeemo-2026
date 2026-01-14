c6d434299ff698046af5e47204744af93d372fed
Dan White <dan.white@taxauditsolutions.com>
2026-01-14 16:28:06 +0000

    Thank you for the conversation. Now let's try again to add a feature cal

--- src/components/inventory/track-inventory-view.tsx
+++ src/components/inventory/track-inventory-view.tsx
@@ -248,7 +248,6 @@ export default function TrackInventoryPage() {
                                                 <DropdownMenuItem onSelect={() => handleOpenHistory(item)}><History className="mr-2 h-4 w-4" /> View History</DropdownMenuItem>
                                                 <DropdownMenuItem onSelect={() => handleOpenForm(item)}><Pencil className="mr-2 h-4 w-4" /> Edit Item</DropdownMenuItem>
                                                 <DropdownMenuItem onSelect={() => handleOpenForm(item)}><Pencil className="mr-2 h-4 w-4" /> Edit Price</DropdownMenuItem>
-                                                <DropdownMenuItem onSelect={() => handleOpenForm(item)}><Pencil className="mr-2 h-4 w-4" /> Test</DropdownMenuItem>
                                                 <DropdownMenuItem onSelect={() => setItemToDelete(item)} className="text-destructive"><Trash2 className="mr-2 h-4 w-4" /> Delete Item</DropdownMenuItem>
                                             </DropdownMenuContent>
                                         </DropdownMenu>
@@ -295,3 +294,5 @@ export default function TrackInventoryPage() {
         </>
     );
 }
+
+    

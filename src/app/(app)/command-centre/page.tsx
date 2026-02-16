c5f7265f782ce458997ed625fee297d0ed6c1118
Dan White <dan.white@taxauditsolutions.com>
2026-02-16 13:54:51 +0000

    Try fixing this error: `Runtime ReferenceError: Badge is not defined. Er

--- src/app/(app)/command-centre/page.tsx
+++ src/app/(app)/command-centre/page.tsx
@@ -12,6 +12,7 @@ import {
     CardFooter 
 } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
+import { Badge } from '@/components/ui/badge';
 import { Input } from '@/components/ui/input';
 import { 
     ArrowLeft, 

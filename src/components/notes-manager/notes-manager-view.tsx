0d7a2d72326a12eb4b2387758017571e31dff264
Dan White <dan.white@taxauditsolutions.com>
2026-02-17 12:48:40 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/components/notes-manager/notes-manager-view.tsx
+++ src/components/notes-manager/notes-manager-view.tsx
@@ -1,7 +1,2 @@
-
-// This component has been deprecated.
-// All file and note management is now handled by the Notes feature and Document Manager.
-// This file is safe to be deleted in a future cleanup.
-export function NotesManagerView() {
-  return null;
-}
+'use client';
+export function NotesManagerView() { return null; }
